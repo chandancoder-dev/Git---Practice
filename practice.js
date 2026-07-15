46a5b78196b67241200280026023d0b07f2904ca
Chandan K R <chandancoder.dev@gmail.com>
2026-07-15 23:05:25 +0530

    commiting the code which i learnt

--- practice.js
+++ practice.js
@@ -1,3 +1,15 @@
 let name = "chandan";
 console.log(name);
-consol.log("git practice 2 ");
+console.log("git practice 2 ");
+// Day 3 Git Practice
+
+const name = "Chandan";
+const role = "Repository Manager";
+
+function greetUser(name) {
+    console.log(`Hello ${name}! Welcome to Git Practice.`);
+}
+
+greetUser(name);
+
+console.log(`Current Role: ${role}`);
