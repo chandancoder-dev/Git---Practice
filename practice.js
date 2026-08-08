12086031f824b13e91eb5153f8492bbf20726fb3
Chandan K R <chandancoder.dev@gmail.com>
2026-08-08 21:59:43 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -7,7 +7,7 @@ console.log(name);
 console.log("Git Practice 2");
 
 function greetUser(userName) {
-    console.log(`Hello ${userName}! Welcome to Git Practice.`);
+  console.log(`Hello ${userName}! Welcome to Git Practice.`);
 }
 
 greetUser(name);
@@ -19,19 +19,18 @@ const skills = ["HTML", "CSS", "JavaScript", "Git", "GitHub"];
 console.log("Learning Skills:");
 
 skills.forEach((skill, index) => {
-    console.log(`${index + 1}. ${skill}`);
+  console.log(`${index + 1}. ${skill}`);
 });
 
 console.log("Keep learning and keep committing! 🚀");
-
 
 // Day 5 Git Practice
 
 const skills = ["HTML", "CSS", "JavaScript", "Git", "GitHub"];
 
-skills.forEach(skill => {
-    console.log(`Learning: ${skill}`);
+skills.forEach((skill) => {
+  console.log(`Learning: ${skill}`);
 });
 
 console.log("Keep learning and keep committing,!🚀");
-console.log("Git Practice 4");
+console.log("Git Practice 5");
